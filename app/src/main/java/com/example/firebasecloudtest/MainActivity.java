fef4cb75757aa62dbff4fbff2f04140596bda7f8
Ali Hathat <icrafter4999@hotmail.com>
2021-06-01 15:50:30 +0300

    comment check test

--- app/src/main/java/com/example/firebasecloudtest/MainActivity.java
+++ app/src/main/java/com/example/firebasecloudtest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //this comment is made to check A C P commands
     }
 }
